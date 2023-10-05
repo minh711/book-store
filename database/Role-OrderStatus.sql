6adcdbd0cbc9fc4ea51d3cb2f6774b3c94f7dfe3
minh711 <109033167+minh711@users.noreply.github.com>
2023-10-05 20:59:47 +0700

    Revert "Delete IDENTIFY_INSERT because already have it on CreateDatabase.sql"
    This reverts commit 4eb1dfca29068dcc136dd531173f3e8881fb2763.

--- database/Role-OrderStatus.sql
+++ database/Role-OrderStatus.sql
@@ -1,13 +1,17 @@
 ﻿USE [SWP_v0]
 GO
+SET IDENTITY_INSERT [OrderStatus] ON;
 INSERT [dbo].[OrderStatus] ([id], [status]) VALUES (1, N'Đang xử lý')
 INSERT [dbo].[OrderStatus] ([id], [status]) VALUES (2, N'Đã tiếp nhận')
 INSERT [dbo].[OrderStatus] ([id], [status]) VALUES (3, N'Đang giao')
 INSERT [dbo].[OrderStatus] ([id], [status]) VALUES (4, N'Thành công')
 INSERT [dbo].[OrderStatus] ([id], [status]) VALUES (5, N'Đã hủy')
+SET IDENTITY_INSERT [OrderStatus] OFF;
 GO
+SET IDENTITY_INSERT [Role] ON;
 INSERT [dbo].[Role] ([id], [role]) VALUES (1, N'Khách hàng')
 INSERT [dbo].[Role] ([id], [role]) VALUES (2, N'Quản lý Sách')
 INSERT [dbo].[Role] ([id], [role]) VALUES (3, N'Quản lý Đơn')
 INSERT [dbo].[Role] ([id], [role]) VALUES (4, N'Admin')
+SET IDENTITY_INSERT [Role] OFF;
 GO
