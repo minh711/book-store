dc13f88dd1d4900fd6a0707947f64c4faaf1aaae
minh711 <109033167+minh711@users.noreply.github.com>
2023-10-05 21:10:08 +0700

    Delete SET IDENTIFY_INSERT because it no use and make errors.

--- database/CreateDatabase.sql
+++ database/CreateDatabase.sql
@@ -3,14 +3,12 @@ CREATE TABLE Publisher (
     publisher NVARCHAR(100) NOT NULL,
     isAvailable BIT NOT NULL
 )
-SET IDENTITY_INSERT Publisher ON;
 
 CREATE TABLE [Language] (
     id INT IDENTITY(1,1) PRIMARY KEY,
     language NVARCHAR(100) NOT NULL,
     isAvailable BIT NOT NULL
 )
-SET IDENTITY_INSERT [Language] ON;
 
 CREATE TABLE Book (
     id INT IDENTITY(1,1) PRIMARY KEY,
@@ -26,46 +24,39 @@ CREATE TABLE Book (
     publisherId INT FOREIGN KEY REFERENCES Publisher(id),
     languageId INT FOREIGN KEY REFERENCES [Language](id)
 )
-SET IDENTITY_INSERT Book ON;
 
 CREATE TABLE BookPicture (
     id INT IDENTITY(1,1) PRIMARY KEY,
     picture VARCHAR(100) NOT NULL,
     bookId INT FOREIGN KEY REFERENCES Book(id)
 )
-SET IDENTITY_INSERT BookPicture ON;
     
 CREATE TABLE Author (
     id INT IDENTITY(1,1) PRIMARY KEY,
     author NVARCHAR(100) NOT NULL,
     isAvailable BIT NOT NULL
 )
-SET IDENTITY_INSERT Author ON;
 
 CREATE TABLE BookAuthor (
     bookId INT FOREIGN KEY REFERENCES Book(id),
     authorId INT FOREIGN KEY REFERENCES Author(id)
 )
-SET IDENTITY_INSERT BookAuthor ON;
 
 CREATE TABLE Genre (
     id INT IDENTITY(1,1) PRIMARY KEY,
     genre NVARCHAR(100) NOT NULL,
     isAvailable BIT NOT NULL
 )
-SET IDENTITY_INSERT Genre ON;
     
 CREATE TABLE BookGenre (
     bookId INT FOREIGN KEY REFERENCES Book(id),
     genreId INT FOREIGN KEY REFERENCES Genre(id)
 )
-SET IDENTITY_INSERT BookGenre ON;    
 
 CREATE TABLE Role (
     id INT IDENTITY(1,1) PRIMARY KEY,
     role NVARCHAR(50) NOT NULL
-)
-SET IDENTITY_INSERT Role ON;    
+) 
     
 CREATE TABLE Account (
     id INT IDENTITY(1,1) PRIMARY KEY,
@@ -80,7 +71,6 @@ CREATE TABLE Account (
     isAvailable BIT NOT NULL,
     roleId INT FOREIGN KEY REFERENCES [Role](id)
 )
-SET IDENTITY_INSERT Account ON;   
 
 CREATE TABLE Address (
     id INT IDENTITY(1,1) PRIMARY KEY,
@@ -89,13 +79,11 @@ CREATE TABLE Address (
     [address] NVARCHAR(500) NOT NULL,
     customerId INT FOREIGN KEY REFERENCES Account(id)
 )
-SET IDENTITY_INSERT Address ON;   
 
 CREATE TABLE NotificationType (
     id INT IDENTITY(1,1) PRIMARY KEY,
     [type] NVARCHAR(50) NOT NULL
 )
-SET IDENTITY_INSERT NotificationType ON;   
     
 CREATE TABLE Notification (
     id INT IDENTITY(1,1) PRIMARY KEY,
@@ -103,7 +91,6 @@ CREATE TABLE Notification (
     isViewed BIT NOT NULL,
     notificationTypeId INT FOREIGN KEY REFERENCES NotificationType(id)
 )
-SET IDENTITY_INSERT Notification ON;   
     
 CREATE TABLE Rating (
     id INT IDENTITY(1,1) PRIMARY KEY,
@@ -111,14 +98,12 @@ CREATE TABLE Rating (
     comment NVARCHAR(500),
     customerId INT FOREIGN KEY REFERENCES Account(id)
 )
-SET IDENTITY_INSERT Rating ON;   
 
 CREATE TABLE Distributor (
     id INT IDENTITY(1,1) PRIMARY KEY,
     distributor NVARCHAR(100) NOT NULL,
     isAvailable BIT NOT NULL
 )
-SET IDENTITY_INSERT Distributor ON;   
     
 CREATE TABLE BookTransaction (
     bookId INT FOREIGN KEY REFERENCES Book(id),
@@ -127,7 +112,6 @@ CREATE TABLE BookTransaction (
     quantity INT NOT NULL,
     [date] DATETIME NOT NULL
 )
-SET IDENTITY_INSERT BookTransaction ON;   
 
 CREATE TABLE Cart (
     id INT IDENTITY(1,1) PRIMARY KEY,
@@ -135,7 +119,6 @@ CREATE TABLE Cart (
     bookId INT FOREIGN KEY REFERENCES Book(id),
     customerId INT FOREIGN KEY REFERENCES Account(id)
 )
-SET IDENTITY_INSERT Cart ON;   
 
 CREATE TABLE [Order] (
     id INT IDENTITY(1,1) PRIMARY KEY,
@@ -150,13 +133,11 @@ CREATE TABLE [Order] (
     [date] DATETIME NOT NULL,
     customerId INT FOREIGN KEY REFERENCES Account(id)
 )
-SET IDENTITY_INSERT [Order] ON;   
 
 CREATE TABLE OrderStatus (
     id INT IDENTITY(1,1) PRIMARY KEY,
     status NVARCHAR(50) NOT NULL
 )
-SET IDENTITY_INSERT OrderStatus ON;   
     
 CREATE TABLE OrderStatusDetail (
     id INT IDENTITY(1,1) PRIMARY KEY,
@@ -164,7 +145,6 @@ CREATE TABLE OrderStatusDetail (
     orderId INT FOREIGN KEY REFERENCES [Order](id),
     orderStatusId INT FOREIGN KEY REFERENCES OrderStatus(id)
 )
-SET IDENTITY_INSERT OrderStatusDetail ON;   
 
 CREATE TABLE OrderDetail (
     orderId INT FOREIGN KEY REFERENCES [Order](id),
@@ -173,4 +153,3 @@ CREATE TABLE OrderDetail (
     salePrice INT NOT NULL,
     price INT NOT NULL
 )
-SET IDENTITY_INSERT OrderDetail ON;   
