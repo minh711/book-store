728d77a70bf4ec8ef431abf218fb5a98418c9106
minh711 <109033167+minh711@users.noreply.github.com>
2023-10-16 16:50:08 +0700

    Add addNew, update and delete methods.

--- src/main/java/DAOs/DBModelDAOs/DistributorDAO.java
+++ src/main/java/DAOs/DBModelDAOs/DistributorDAO.java
@@ -24,7 +24,7 @@ public class DistributorDAO {
     }
     
     /**
-     * Get all Distributors.
+     * Get all available Distributors.
      * 
      * @return Distributor array.
      * @author MinhTD
@@ -34,7 +34,7 @@ public class DistributorDAO {
         String sql
                 = "SELECT * "
                 + "FROM Distributor "
-                + "WHERE isAvailable = 1";
+                + "WHERE isAvailable = 1"; // 1 means True in BIT data type
         try {
             ps = conn.prepareStatement(sql);
             rs = ps.executeQuery();
@@ -50,5 +50,74 @@ public class DistributorDAO {
         Distributor[] arr = new Distributor[ls.size()];
         ls.toArray(arr);
         return arr;
+    }
+    
+    /**
+     * Add new Distributor.
+     * 
+     * Since Distributor only have one attribute that matter, the isAvailable 
+     * attribute is always True when first added, so that one parameter is 
+     * enough.
+     * @param distributor Name of the distributor.
+     * @return Number of rows effected. 0 mean failed.
+     * @author MinhTD
+     */
+    public int addNew(String distributor) {
+        int result = 0;
+        String sql 
+                = "INSERT INTO Distributor "
+                + "(distributor, isAvailable) "
+                + "VALUES "
+                + "(?, 1)"; // 1 is True value for BIT in isAvailable column
+        try {
+            ps = conn.prepareStatement(sql);
+            ps.setNString(1, distributor);
+            result = ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(DistributorDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+    
+    /**
+     * Update a Distributor.
+     * 
+     * Since it only possible to update available Distributors, so there is no
+     * need to include isAvailable attribute in here.
+     * @param distributor Distributor object, to get the id and distributor's name.
+     * @return Number of rows effected. 0 mean failed.
+     * @author MinhTD
+     */
+    public int update(Distributor distributor) {
+        int result = 0;
+        String sql 
+                = "UPDATE Distributor "
+                + "SET distributor = ? "
+                + "WHERE id = ?";
+        try {
+            ps = conn.prepareStatement(sql);
+            ps.setNString(1, distributor.getDistributor());
+            ps.setInt(2, distributor.getId());
+            result = ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(DistributorDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+    
+    public int delete(int id) {
+        int result = 0;
+        String sql
+                = "UPDATE Distributor "
+                + "SET isAvailable = 0" // 0 mean False in BIT data type
+                + "WHERE id = ?"; 
+        try {
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            result = ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(DistributorDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
     }
 }
